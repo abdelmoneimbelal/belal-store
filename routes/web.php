f8f584dd68de40a3b7e9869137afdba73cf55503
Abdelmoneim Belal <38110521+abdelmoneimbelal@users.noreply.github.com>
2025-08-12 00:37:14 +0300

    admin login

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\Forntend\ForntendController;
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Backend\BackendController;
+use App\Http\Controllers\Forntend\ForntendController;
 
 
 
@@ -22,4 +23,38 @@ Route::get('/checkout', [ForntendController::class, 'checkout'])->name('frontend
 
 Auth::routes();
 
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
+    Route::group(['middleware' => 'guest'], function () {
+        Route::get('/login', [BackendController::class, 'login'])->name('login');
+        Route::get('/forgot-password', [BackendController::class, 'forgot_password'])->name('forgot_password');
+    });
+
+    Route::group(['middleware' => ['roles', 'role:admin|supervisor']], function (){
+        Route::get('/', [BackendController::class, 'index'])->name('index_route');
+        Route::get('/index', [BackendController::class, 'index'])->name('index');
+        Route::get('/account_settings', [BackendController::class, 'account_settings'])->name('account_settings');
+        Route::post('/admin/remove-image', [BackendController::class, 'remove_image'])->name('remove_image');
+        Route::patch('/account_settings', [BackendController::class, 'update_account_settings'])->name('update_account_settings');
+        Route::post('/product_categories/remove-image', [ProductCategoriesController::class, 'remove_image'])->name('product_categories.remove_image');
+        Route::resource('product_categories', ProductCategoriesController::class);
+        Route::post('/products/remove-image', [ProductController::class, 'remove_image'])->name('products.remove_image');
+        Route::resource('products', ProductController::class);
+        Route::resource('tags', TagController::class);
+        Route::resource('product_coupons', ProductCouponController::class);
+        Route::resource('product_reviews', ProductReviewController::class);
+        Route::post('/customers/remove-image', [CustomerController::class, 'remove_image'])->name('customers.remove_image');
+        Route::get('/customers/get_customers', [CustomerController::class, 'get_customers'])->name('customers.get_customers');
+        Route::resource('customers', CustomerController::class);
+        Route::resource('customer_addresses', CustomerAddressController::class);
+        Route::post('/supervisors/remove-image', [SupervisorController::class, 'remove_image'])->name('supervisors.remove_image');
+        Route::resource('supervisors', SupervisorController::class);
+        Route::resource('orders', OrderController::class);
+        Route::resource('countries', CountryController::class);
+        Route::get('states/get_states', [StateController::class, 'get_states'])->name('states.get_states');
+        Route::resource('states', StateController::class);
+        Route::get('cities/get_cities', [CityController::class, 'get_cities'])->name('cities.get_cities');
+        Route::resource('cities', CityController::class);
+        Route::resource('shipping_companies', ShippingCompanyController::class);
+        Route::resource('payment_methods', PaymentMethodController::class);
+    });
+});
