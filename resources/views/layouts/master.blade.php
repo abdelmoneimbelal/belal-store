0352f27a8b783faf6825c341871fd275014d43a4
Abdelmoneim Belal <38110521+abdelmoneimbelal@users.noreply.github.com>
2025-08-11 16:33:57 +0300

    admin files

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,6 +14,7 @@
         <div class="container">
             @yield('content')
         </div>
+        
         @include('layouts.footer')
         <!-- JavaScript files-->
         @include('layouts.scripts')
