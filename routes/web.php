cc698c03677a2d91ead71e824350334c76b3828b
Abdelmoneim Belal <38110521+abdelmoneimbelal@users.noreply.github.com>
2025-08-11 11:11:14 +0300

    update routes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,25 @@
 <?php
 
+use App\Http\Controllers\Forntend\ForntendController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('frontend.index');
-});
+
+
+
+Route::get('/', [ForntendController::class, 'index'])->name('index');
+
+Route::get('/shop', [ForntendController::class, 'shop'])->name('shop');
+
+Route::get('/detail', [ForntendController::class, 'detail'])->name('detail');
+
+Route::get('/cart', [ForntendController::class, 'cart'])->name('cart');
+
+Route::get('/checkout', [ForntendController::class, 'checkout'])->name('checkout');
+
+// Route::get('/test', [ForntendController::class, 'test'])->name('test');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
